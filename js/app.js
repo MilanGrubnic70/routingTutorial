d529cb9bcae4ebeef2367a787570ec0fc3a13b4e
Milan Grubnić <MilanG@aol.com>
2016-03-13 17:26:29 -0500

    added routing in app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 var app = angular.module('GalleryApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+
+    .when('/', {
+        controller: 'HomeController',
+        templateUrl: 'views/home.html'
+    })
+
+    .otherwise({
+        redirectTo: '/'
+    });
+
+});
